03d35b0886be4a041dd07a35618e6ba8e5811f5e
Tanjim H. <contact@audacioustux.com>
2026-01-16 23:32:54 +0600

    chore: Remove `op/plugins.sh` sourcing and `AWS_PAGER` session variable from shell configuration.

--- home/shell.nix
+++ home/shell.nix
@@ -1,9 +1,3 @@
-{ lib, ... }:
-let
-  sourceFiles = [
-    "~/.config/op/plugins.sh"
-  ];
-in
 {
   programs.zsh = {
     enable = true;
@@ -11,19 +5,11 @@ in
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    sessionVariables = {
-      AWS_PAGER = "";
-    };
-
     shellAliases = {
       la = "ls -la";
       ".." = "cd ..";
       "nix-switch" = "sudo darwin-rebuild switch --flake ~/.config/nix";
     };
-
-    initContent = lib.concatMapStrings (f: ''
-      [ -f ${f} ] && source ${f}
-    '') sourceFiles;
 
     oh-my-zsh = {
       enable = true;
