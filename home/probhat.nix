808f0266053d660cb31b9d633e7421bfd73493ec
Tanjim H. <contact@audacioustux.com>
2026-02-26 21:24:21 +0600

    chore: update probhat-macos flake input and remove home-manager keyboard layout rsync configuration.

--- home/probhat.nix
+++
@@ -1,7 +0,0 @@
-{ lib, pkgs, ... }:
-
-{
-  home.activation.copyProbhat = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -a --chmod=D755,F644 ${./assets/probhat}/ "$HOME/Library/Keyboard Layouts/"
-  '';
-}
